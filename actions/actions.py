06aa588c49de7c21ad712c95d85c076f376cb378
SuienS <ravidu.rocks@gmail.com>
2021-06-09 16:54:35 +0530

    Room Booking

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,13 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List, Union
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, UserUtteranceReverted
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +28,38 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class BookRoomInfo(FormAction):
+    def name(self) -> Text:
+        return "form_book_room"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        return ["room_type"]
+
+    def submit(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict]:
+
+        # utter submit template
+        dispatcher.utter_message(template="utter_room_booked",
+                                 room_type=tracker.get_slot('room_type'))
+        return []
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+
+        return {
+            "room_type": self.from_entity(entity="room_type", intent="type_select_room")
+        }
+
+class ResetSlots(Action):
+
+    def name(self):
+        return "action_reset_slots"
+
+    def run(self, dispatcher, tracker, domain):
+        return [SlotSet("room_type", None)]
+
